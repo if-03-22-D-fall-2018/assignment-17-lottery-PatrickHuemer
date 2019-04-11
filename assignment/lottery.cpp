90d886a625dee7bbe7ede46f23e0614498feeac8
Huemer Patrick <if170053@WS8A15.htl-leonding.ac.at>
2019-04-11 13:03:33 +0200

    init

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -10,3 +10,27 @@
  * Implementation of a library for analyzing lottery tips.
  *-----------------------------------------------------------------------------
 */
+#include <stdio.h>
+#include "lottery.h"
+
+#define 	UUID_LEN   40
+#define 	MAX_TIP_LEN   17
+#define 	MAX_LINE_LEN   (UUID_LEN + 1 + MAX_TIP_LEN + 1)
+
+
+bool 	init_lottery (const char *csv_file, char csv_separator){
+  FILE* fd = fopen("tips1.csv","r");
+  return true;
+}
+bool 	get_tip (int tip_number, int tip[TIP_SIZE]){
+  return true;
+}
+bool 	set_drawing (int drawing_numbers[TIP_SIZE]){
+  return true;
+}
+int 	get_tip_result (int tip_number){
+  return -1;
+}
+int 	get_right_tips_count (int right_digits_count){
+  return -1;
+}
