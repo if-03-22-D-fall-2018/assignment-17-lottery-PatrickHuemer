befaada4ccba7f26e128f052f4d5b53109441f7a
Huemer Patrick <if170053@WS8A15.htl-leonding.ac.at>
2019-06-03 09:45:32 +0200

    ca

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -13,21 +13,84 @@
 #include <stdio.h>
 #include "lottery.h"
 
+FILE* fd;
+int draw[TIP_SIZE];
+char seperator;
+
 #define 	UUID_LEN   40
 #define 	MAX_TIP_LEN   17
 #define 	MAX_LINE_LEN   (UUID_LEN + 1 + MAX_TIP_LEN + 1)
 
 
 bool 	init_lottery (const char *csv_file, char csv_separator){
-  FILE* fd = fopen("tips1.csv","r");
-  return true;
+  fd = fopen(csv_file,"r");
+  seperator = csv_separator;
+
+  for (int i = 0; i < TIP_SIZE; i++) {
+    draw[i] = 0;
+  }
+  return fd != 0;
 }
+
+int char_to_int(char* chars, int count){
+  int number = 0;
+
+  for (int i = 0; i < count; i++) {
+    number = number * 10 + chars[i] - '0';
+  }
+  return number;
+}
+
+int search_start(char* line){
+  for (int i = 0; i < MAX_LINE_LEN; i++) {
+    if (line[i] == seperator) {
+      return i+1;
+    }
+  }
+  return -1;
+}
+
 bool 	get_tip (int tip_number, int tip[TIP_SIZE]){
+  char line[MAX_LINE_LEN];
+  fseek(fd, 0, SEEK_SET);
+
+  if (tip_number < 0) {
+    return false;
+  }
+
+  for (int i = 0; i < count; i++) {
+    if (fgets(line, MAX_LINE_LEN, fd) == 0) {
+      return false;
+    }
+  }
+
+  int idx = 0, count = 0;
+  for (int i = 0; i < count; i++) {
+    char numbers[2];
+
+    if (line[i] == seperator || line[i] == '\n' || line[i] == '\0') {
+      tip[idx++] = char_to_int(numbers, count);
+      count = 0;
+      if (index == TIP_SIZE) {
+        break;
+      }
+    }
+    else{
+      numbers[count++] = line[i];
+    }
+  }
   return true;
 }
 bool 	set_drawing (int drawing_numbers[TIP_SIZE]){
+  for (int i = 0; i < TIP_SIZE; i++) {
+    if (drawing_numbers[i] < 1 || drawing_numbers[i] > 45) {
+      return false;
+    }
+    drawing[i] = drawing_numbers[i];
+  }
   return true;
 }
+
 int 	get_tip_result (int tip_number){
   return -1;
 }
